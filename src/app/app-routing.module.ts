8929a2163f3dc81fb70da55ca7bd7a98214fd17c
Adovax <mailstodeepakdev@gmail.com>
2020-12-12 11:30:29 +0530

    f

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
-
+import { RegistrationComponent } from './registration/registration.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+const routes: Routes = [
+  { path: '', component: RegistrationComponent },
+  { path: 'Registration', component: RegistrationComponent },
+  { path: 'Dashboard', component: DashboardComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
