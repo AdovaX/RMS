f93945a53e0770b444f4e59473e8613c0c44178a
Adovax <mailstodeepakdev@gmail.com>
2020-12-15 11:30:16 +0530

    profile design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RegistrationComponent } from './registration/registration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+
 const routes: Routes = [
   { path: '', component: RegistrationComponent },
   { path: 'Registration', component: RegistrationComponent },
-  { path: 'Dashboard', component: DashboardComponent }
+  { path: 'Dashboard', component: DashboardComponent },
+  { path: 'Profile', component: ProfileComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
