d8b39d14bd1d83bb55c78efa234c186fa67ee2b5
Adovax <mailstodeepakdev@gmail.com>
2020-12-17 19:35:35 +0530

    new design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { UserHeaderComponent } from './user-header/user-header.component';
 import {MatButtonModule} from '@angular/material/button';
 import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTabsModule} from '@angular/material/tabs';
 
  
  @NgModule({
@@ -43,7 +44,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatTabsModule
 
   ],
   providers: [{
