44fd8bac73305e7f8ed689adb023a3d557b1e8fc
unknown <udhaysekar@gmail.com>
2020-12-18 14:28:45 +0900

    backend added_v2

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,5 @@ app.use('/accounts', require('./accounts/accounts.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8080;
 app.listen(port, () => console.log('Server listening on port ' + port));
